7eef2d864422d0555a73fee4022e9d5d6fa6ab7c
ROKUN KHAN <rokunkhan1@gmail.com>
2025-07-30 23:42:50 -0400

    Update serial_logger.py

--- serial_logger.py
+++ serial_logger.py
@@ -1,15 +1,14 @@
 import serial
 import time
 
-# Replace COM7 with your actual COM port if needed
 ser = serial.Serial('COM7', 9600)
-time.sleep(2)  # Allow serial connection to settle
+time.sleep(2)  
 
 try:
     while True:
         data = ser.readline().decode('utf-8').strip()
         print(data)
-        time.sleep(1)  # Optional: slow down printing
+        time.sleep(1) 
 
 except KeyboardInterrupt:
     print("\nLogging stopped by user.")
